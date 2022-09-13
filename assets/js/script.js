4beace18f7c74738ae7359a7e4b2d893069e682e
Brian Campbell <brian.campbell003@gmail.com>
2022-09-12 23:30:36 -0500

    stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
-// WRITE CARD CLICKER FUNCTIONS
+// WRITE CARD CLICK CARD FUNCTIONS
 
 // WRITE CODE FOR TIMER
+function startTimer() {
+    const timeEl = document.getElementById("timer");
+    let secondsLeft = 59;
+    let timerInterval = setInterval(function() {
+        timeEl.innerHTML = "0:" + secondsLeft + "s";
+        secondsLeft--;
+            
+        if(secondsLeft <= 10) {
+            timeEl.setAttribute("style", "color: darkred; font-size: 20px; font-weight: bold");
+        }
+
+        if(secondsLeft <= 0) {
+        clearInterval(timerInterval);
+        killPage();
+        }
+        }, 1000);
+};
+
+startTimer();
