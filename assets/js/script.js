a67edb6aaed2c96a73eecd81be6fee771fcdeff5
Brian Campbell <brian.campbell003@gmail.com>
2022-09-18 01:36:21 -0500

    all cards click, some cards populated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,38 @@
 // WRITE CARD CLICK CARD FUNCTIONS
 var harold = document.querySelector("#harold");
 harold.addEventListener("click", showHarold);
-
 function showHarold() {
     $('#haroldModal').modal('show')
 }
 
 var hated = document.querySelector("#hated");
 hated.addEventListener("click", showHated);
-
 function showHated() {
     $('#hatedModal').modal('show')
+}
+
+var hideous = document.querySelector("#hideous");
+hideous.addEventListener("click", showHideous);
+function showHideous() {
+    $('#hideousModal').modal('show')
+}
+
+var hunter = document.querySelector("#hunter");
+hunter.addEventListener("click", showHunter);
+function showHunter() {
+    $('#hunterModal').modal('show')
+}
+
+var sjod = document.querySelector("#sjod");
+sjod.addEventListener("click", showSjod);
+function showSjod() {
+    $('#sjodModal').modal('show')
+}
+
+var six = document.querySelector("#six");
+six.addEventListener("click", showSix);
+function showSix() {
+    $('#sixModal').modal('show')
 }
 
 
@@ -25,11 +47,10 @@ for (let i = 0; i < closeBtns.length; i++) {
 function closeModal() {
     $('#haroldModal').modal('hide');
     $('#hatedModal').modal('hide');
-//     $('#hunterModal').modal('hide');
-//     $('#hideousModal').modal('hide');
-//     $('#___Modal').modal('hide');
-//     $('#___Modal').modal('hide');
-
+    $('#hideousModal').modal('hide');
+    $('#hunterModal').modal('hide');
+    $('#sjodModal').modal('hide');
+    $('#sixModal').modal('hide');
 }
 
 
